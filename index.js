aaaaad4ab4d124c7cf476249e922fcd4e4e9f62c
airobltd <130511570+airobltd@users.noreply.github.com>
2023-04-25 00:21:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 var net = require('net');
-var server = net.createServer();    
-server.on('connection', handleConnection);
-server.listen(9000, function() {    
-  console.log('server listening to %j', server.address());  
-});
-function handleConnection(conn) {    
-  var remoteAddress = conn.remoteAddress + ':' + conn.remotePort;  
-  console.log('new client connection from %s', remoteAddress);
-  conn.on('data', onConnData);  
-  conn.once('close', onConnClose);  
-  conn.on('error', onConnError);
-  function onConnData(d) {  
-    console.log('connection data from %s: %j', remoteAddress, d);  
-    conn.write(d);  
-  }
-  function onConnClose() {  
-    console.log('connection from %s closed', remoteAddress);  
-  }
-  function onConnError(err) {  
-    console.log('Connection %s error: %s', remoteAddress, err.message);  
-  }  
-}
+ 
+var HOST = '127.0.0.1';
+var PORT = 6969;
+ 
+// Creaiamo un istanza del server, e concateniamo la funzione listen
+// La funzione passata a net.createServer() diventerà l'event handler per l'evento 'connection'
+// l'oggetto sock che la callback prende come parametro è univoco per ogni connessione.
+net.createServer(function(sock) {
+ 
+    // abbiamo una connessione - un oggetto è assegnato alla connessione automaticamente
+    console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
+ 
+    // Aggiungiamo l'event handler 'data' per questa instanza di socket
+    sock.on('data', function(data) {
+ 
+        console.log('DATA ' + sock.remoteAddress + ': ' + data);
+        // Rispondiamo al client, il client riceverà questi dati dal server.
+        sock.write('You said "' + data + '"');
+ 
+    });
+ 
+    // Aggiungiamo l'event handler 'close' per questa istanza di socket
+    sock.on('close', function(data) {
+        console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
+    });
+ 
+}).listen(PORT, HOST);
+ 
+console.log('Server listening on ' + HOST +':'+ PORT);
