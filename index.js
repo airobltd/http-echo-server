395b1f7e07395252e860f871c484d4b68415249c
airobltd <130511570+airobltd@users.noreply.github.com>
2023-04-25 12:55:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,80 @@
-// Include Nodejs' net module.
-const Net = require('net');
-// The port number and hostname of the server.
-const port = 8080;
-const host = 'localhost';
+#!/usr/bin/env node
+'use strict'
 
-// Create a new TCP client.
-const client = new Net.Socket();
-// Send a connection request to the server.
-client.connect({ port: port, host: host }), function() {
-    // If there is no error, the server has accepted the request and created a new 
-    // socket dedicated to us.
-    console.log('TCP connection established with the server.');
+var getPort = require('get-port')
+var server = require('net').createServer()
 
-    // The client can now send data to the server by writing to its socket.
-    client.write('Hello, server.');
-});
+var cid = 0
 
-// The client can also receive data from the server by reading from its socket.
-client.on('data', function(chunk) {
-    console.log(`Data received from the server: ${chunk.toString()}.`);
-    
-    // Request an end to the connection after the data has been received.
-    client.end();
-});
+module.exports = server // for testing
 
-client.on('end', function() {
-    console.log('Requested an end to the TCP connection');
-});
+onEmit(server, { ignore: ['connection', 'listening', 'error'] }, function (eventName) {
+  console.log('[server] event:', eventName)
+})
+
+server.on('connection', function (c) {
+  var gotData = false
+  var _cid = ++cid
+
+  console.log('[server] event: connection (socket#%d)', _cid)
+
+  onEmit(c, { ignore: ['lookup', 'error'] }, function (eventName) {
+    console.log('[socket#%d] event:', _cid, eventName)
+  })
+
+  c.on('lookup', function (err, address, family) {
+    if (err) {
+      console.log('[socket#%d] event: lookup (error: %s)', _cid, err.message)
+    } else {
+      console.log('[socket#%d] event: lookup (address: %s, family: %s)', _cid, address, family)
+    }
+  })
+
+  c.on('data', function (chunk) {
+    console.log('--> ' + chunk.toString().split('\n').join('\n--> '))
+    if (!gotData) {
+      gotData = true
+      c.write('HTTP/1.1 200 OK\r\n')
+      c.write('Date: ' + (new Date()).toString() + '\r\n')
+      c.write('Connection: close\r\n')
+      c.write('Content-Type: text/plain\r\n')
+      c.write('Access-Control-Allow-Origin: *\r\n')
+      c.write('\r\n')
+      setTimeout(function () {
+        c.end()
+      }, 2000)
+    }
+    c.write(chunk.toString())
+  })
+
+  c.on('error', function (err) {
+    console.log('[socket#%d] event: error (msg: %s)', _cid, err.message)
+  })
+})
+
+server.on('listening', function () {
+  var port = server.address().port
+  console.log('[server] event: listening (port: %d)', port)
+})
+
+server.on('error', function (err) {
+  console.log('[server] event: error (msg: %s)', err.message)
+})
+
+var port = process.argv[2] || process.env.PORT
+
+if (port) {
+  server.listen(port)
+} else {
+  getPort({ port: 3000 }).then(function (port) {
+    server.listen(port)
+  })
+}
+
+function onEmit (emitter, opts, cb) {
+  var emitFn = emitter.emit
+  emitter.emit = function (eventName) {
+    if (opts.ignore.indexOf(eventName) === -1) cb.apply(null, arguments)
+    return emitFn.apply(emitter, arguments)
+  }
+}
