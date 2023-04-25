f7ce014aab64a30a06fec43954a478e882b1835c
airobltd <130511570+airobltd@users.noreply.github.com>
2023-04-25 13:24:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,80 +1,23 @@
-#!/usr/bin/env node
-'use strict'
-
-var getPort = require('get-port')
-var server = require('net').createServer()
-
-var cid = 0
-
-module.exports = server // for testing
-
-onEmit(server, { ignore: ['connection', 'listening', 'error'] }, function (eventName) {
-  console.log('[server] event:', eventName)
-})
-
-server.on('connection', function (c) {
-  var gotData = false
-  var _cid = ++cid
-
-  console.log('[server] event: connection (socket#%d)', _cid)
-
-  onEmit(c, { ignore: ['lookup', 'error'] }, function (eventName) {
-    console.log('[socket#%d] event:', _cid, eventName)
-  })
-
-  c.on('lookup', function (err, address, family) {
-    if (err) {
-      console.log('[socket#%d] event: lookup (error: %s)', _cid, err.message)
-    } else {
-      console.log('[socket#%d] event: lookup (address: %s, family: %s)', _cid, address, family)
-    }
-  })
-
-  c.on('data', function (chunk) {
-    console.log('--> ' + chunk.toString().split('\n').join('\n--> '))
-    if (!gotData) {
-      gotData = true
-      c.write('HTTP/1.1 200 OK\r\n')
-      c.write('Date: ' + (new Date()).toString() + '\r\n')
-      c.write('Connection: close\r\n')
-      c.write('Content-Type: text/plain\r\n')
-      c.write('Access-Control-Allow-Origin: *\r\n')
-      c.write('\r\n')
-      setTimeout(function () {
-        c.end()
-      }, 2000)
-    }
-    c.write(chunk.toString())
-  })
-
-  c.on('error', function (err) {
-    console.log('[socket#%d] event: error (msg: %s)', _cid, err.message)
-  })
-})
-
-server.on('listening', function () {
-  var port = 3000
-  console.log('[server] event: listening (port: %d)', port)
-})
-
-server.on('error', function (err) {
-  console.log('[server] event: error (msg: %s)', err.message)
-})
-
-var port = 3000
-
-if (port) {
-  server.listen(port)
-} else {
-  getPort({ port: 3000 }).then(function (port) {
-    server.listen(port)
-  })
-}
-
-function onEmit (emitter, opts, cb) {
-  var emitFn = emitter.emit
-  emitter.emit = function (eventName) {
-    if (opts.ignore.indexOf(eventName) === -1) cb.apply(null, arguments)
-    return emitFn.apply(emitter, arguments)
+var net = require('net');
+var server = net.createServer();    
+server.on('connection', handleConnection);
+server.listen(9000, function() {    
+  console.log('server listening to %j', server.address());  
+});
+function handleConnection(conn) {    
+  var remoteAddress = conn.remoteAddress + ':' + conn.remotePort;  
+  console.log('new client connection from %s', remoteAddress);
+  conn.on('data', onConnData);  
+  conn.once('close', onConnClose);  
+  conn.on('error', onConnError);
+  function onConnData(d) {  
+    console.log('connection data from %s: %j', remoteAddress, d);  
+    conn.write(d);  
   }
+  function onConnClose() {  
+    console.log('connection from %s closed', remoteAddress);  
+  }
+  function onConnError(err) {  
+    console.log('Connection %s error: %s', remoteAddress, err.message);  
+  }  
 }
